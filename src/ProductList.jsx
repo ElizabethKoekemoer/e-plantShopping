ab5bf0ed5efe4fa06f84ef5fadfc759ecbc13346
ElizabethKoekemoer <emvndm0708@gmail.com>
2026-06-01 13:15:44 +1000

    message

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -271,9 +271,9 @@ function ProductList({ onHomeClick }) {
     };
 
     const calculateTotalQuantity = () => {
-        return CartItems ? 
-        CartItems.reduce((total, item) => total + item.quantity, 0) 
-        : 0;
+        return CartItems ?
+             CartItems.items.reduce((total, item) => total + item.quantity, 0)
+            : 0;
     };
 
     return (
@@ -302,6 +302,7 @@ function ProductList({ onHomeClick }) {
                                 <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute">
                                 </path>
                             </svg>
+                            <span>{calculateTotalQuantity()}</span>
                         </h1>
                     </a>
                     </div>
@@ -326,12 +327,22 @@ function ProductList({ onHomeClick }) {
                                         {/* Display other plant details like description and cost */}
                                         <div className="product-description">{plant.description}</div> {/*Display plant description*/}
                                         <div className="product-cost">{plant.cost}</div> {/*Display plant cost*/}
-                                        <button
-                                            className="product-button"
-                                            onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
-                                        >
-                                            Add to Cart
-                                        </button>
+                                        {addedToCart[plant.name]
+                                            ?
+                                            (<button
+                                                className="product-button.added-to-cart"
+                                            >
+                                                Added to Cart
+                                            </button>)
+                                            :
+                                            (<button
+                                                className="product-button"
+                                                onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
+                                            >
+                                                Add to Cart
+                                            </button>)
+                                        }
+
                                     </div>
                                 ))}
                             </div>
